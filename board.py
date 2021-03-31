b7eba24e2ea66bad015e3243d74b881ab4331bf3
rowlavel <rowlavel@iu.edu>
2021-03-30 22:09:53 -0400

    started on board filling in

--- board.py
+++ board.py
@@ -2,24 +2,39 @@
 # Zach Wilkerson, Rowan Lavelle, Josep Han
 
 import numpy as np
+from functions import *
 
 class Board:
 
-    def __init__(self, initState:str = None):
+    def __init__(self, initState:str = None, turn:bool):
         if initState is None:
             pass # TODO: create a dictionary of piece designations tied to their board location... but have to determine unique pieces
         else:
-            pass # TODO: generate board state based on string input
+            self.state = np.array([p for p in initState])
+            self.turn = turn # true is whites turn, false is blacks turn
+            self.piece_locs = self.init_piece_locs_dict(self.state)
+    
+    def self.init_board(self.state)
 
-    def __repr__(self):
-        pass
+    def __str__(self):
+        s = ""
+        for row in range(ROWS):
+            for col in range(COLS):
+                s += self.state[rc_to_i(row,col)]
+            s += "\n"
+        return s
+    
+    def find_piece(p):
+        return np.where(self.state == p)[0]
 
-    def successor(self, move:str):
+    def successor(self):
         pass
 
     def makeMove(self, move:str):
         pass
-
+    
+    @property
     def isTerminal(self):
-        pass
-
+        if np.count_nonzero(self.state == 'k') == 0: return 1 if self.turn else 0
+        if np.count_nonzero(self.state == 'K') == 0: return 0 if self.turn else 1
+        return -1
