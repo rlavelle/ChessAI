f1a270fbdaa9933150e725ec582729187234077b
rowlavel <rowlavel@iu.edu>
2021-04-10 02:49:27 -0400

    updated converesion function to deal with castling, captures, and disambigious moves

--- functions.py
+++ functions.py
@@ -27,7 +27,7 @@ def valid_index(pos):
 #                         Rdf8 for Rook at col d move to f8
 #                         Qh4e1 for queen at h4 to e1
 #                         Qh4xe1 for queen at h4 takes piece at e1
-def convertFromNotation(move:str):
+def convertFromNotation(move:str, turn:bool):
     yConversion = {
         "a":0,
         "b":1,
@@ -38,6 +38,40 @@ def convertFromNotation(move:str):
         "g":6,
         "h":7
     }
+    
+    # unpacked = (piece,from_col/row, to_row, to_col)
+    if move == 'O-O' or move == 'O-O-O':
+        # king side castle
+
+        # king should be in init position, same with castle
+        # WHITE (at top of board)
+        if board.turn:
+            return ("K", 0, 7)
+        else:
+            return ("k", 7, 7)
+    if move == 'O-O-O':
+        # queen side castle
+
+        # king should be in init position, same with castle
+        if board.turn:
+            return ("K", 0, 0)
+        else:
+            return ("k", 7, 0)
+    if len(move) == 4 and 'x' in move:
+        # capture move
+        if move[0] not in "NRBQK":
+            # pawn capture 
+            return ("P", yConversion[move[0]], int(move[3])-1, yConversion[move[2]])
+        else:
+            return (move[0], int(move[3])-1, yConversion[move[2]])
+    if len(move) == 4 and 'x' not in move:
+        # disambiguating moves (cant be a pawn unless a capture)
+        # if disambiguating in the col
+        if move[1].isalpha():
+            return (move[0], yConversion[move[1]], int(move[3])-1, yConversion[move[2]])
+        # if disambiguating in the row
+        else:
+            return (move[0], int(move[1])-1, int(move[3])-1, yConversion[move[2]])
     if len(move) == 3:
         return (move[0], int(move[2])-1, yConversion[move[1]])
     else:
