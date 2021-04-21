000c54b7383803088de44cb0302eeab266356637
rowlavel <rowlavel@iu.edu>
2021-04-21 15:03:46 -0400

    added depth and base player

--- players.py
+++ players.py
@@ -10,7 +10,6 @@ import time
 import math
 from opening import OpenAI
 
-MAX_DEPTH = 5
 
 class RandomPlayer:
 
@@ -22,10 +21,11 @@ class RandomPlayer:
 
 class CBRPlayer(AI):
 
-    def __init__(self, player:bool, verbose=True):
+    def __init__(self, player:bool, verbose=True, depth = 5):
         super().__init__(player, verbose)
         self.open_ai = OpenAI()
         self.use_open = True
+        self.depth = depth
 
     def makeMove(self, board:Board):
         # IDS version of alpha beta
@@ -47,7 +47,7 @@ class CBRPlayer(AI):
             self.use_open = False
             
             k = 1
-            while k < MAX_DEPTH:
+            while k < self.depth:
                 self.count = 0
                 self.pruned = 0
                 
@@ -143,3 +143,96 @@ class CBRPlayer(AI):
                 if alpha >= beta: break 
         
         return best_move, best_score
+
+class BasePlayer(AI):
+
+    def __init__(self, player:bool, verbose=True, depth = 5):
+        super().__init__(player, verbose)
+        self.depth = depth
+        
+    def makeMove(self, board:Board):
+        # IDS version of alpha beta
+
+        k = 1
+        while k < self.depth:
+            self.count = 0
+            
+            start = time.time()
+
+            best_move, score = self.alpha_beta_minimax(board=board,
+                                                        depth=k,
+                                                        alpha=-math.inf,
+                                                        beta=math.inf)
+            end = time.time()
+
+            if self.verbose:
+                print(f'depth: {k}, runtime: {end-start}, states visited: {self.count}')
+
+            k += 1
+        return best_move,score
+
+    def alpha_beta_minimax(self,board,depth,alpha,beta):
+        self.count += 1
+        terminal = board.outcome()
+
+        # base cases (bottom of the minimax tree)
+        if terminal:
+            if terminal.winner == self.player:
+                return None, self.player_win_score
+            if terminal != self.player:
+                return None, self.player_lose_score
+        if depth == 0:
+            return None, self.heuristic(board)
+        
+        # recursive case
+        best_move = None
+        
+        moves = list(board.legal_moves)
+
+        # maximize
+        if board.turn == self.player:
+            best_score = -math.inf
+            for move in moves:
+                oldBoard = board.copy()
+                board.push(move)
+                _,score = self.alpha_beta_minimax(board=board,
+                                            depth=depth-1,
+                                            alpha=alpha,
+                                            beta=beta)
+                board.pop()
+                if score > best_score:
+                    best_score, best_move = score, move
+
+                alpha = max(alpha,score)
+                if alpha >= beta: break            
+        # minimize
+        else:
+            best_score = math.inf
+            for move in moves:
+                board.push(move)
+                _,score = self.alpha_beta_minimax(board=board,
+                                                    depth=depth-1,
+                                                    alpha=alpha,
+                                                    beta=beta)
+                board.pop()
+                if score < best_score:
+                    best_score, best_move = score, move
+
+                beta = min(beta,score)
+                if alpha >= beta: break 
+        
+        return best_move, best_score
+    
+
+
+    
+
+if __name__ == "__main__":
+    board = chess.Board(fen='3r1k2/1pQ5/2B2pp1/p7/2P1P3/2K2P2/PP6/7R w - - 1 38')
+    print(board)
+
+    cbr = BasePlayer(chess.WHITE, verbose=True)
+
+    move,score = cbr.makeMove(board)
+    print(move)
+    print(score)
